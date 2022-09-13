c1f65a5e7edb132b96e20829faa75d25ff0ffa78
Vlad <vladvitalaru@yahoo.com>
2022-09-13 15:04:54 -0700

    Init

---
+++ ranking.py
@@ -0,0 +1,35 @@
+#Vlad Vitalaru
+#CS 454 Fall 2022
+#Assignment 2
+
+class TF_IDF():
+    def __init__(self,dataFile):
+        pass
+    
+    def tf_idf(self, Q, k):
+        pass
+    
+    def relevance(self, d , Q):
+        pass
+    
+    
+    def tf(self, d, t):
+        pass
+    
+
+class BM_25():
+    def __init__(self, dataFile):
+        pass
+    
+    
+    def bm25(self, query, k):
+        pass
+
+
+
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+	main()

c84558f85bc4125423eb70f83c7a9815c3d71175
Vlad <vladvitalaru@yahoo.com>
2022-09-15 16:48:23 -0700

    Implemented TF

--- ranking.py
+++ ranking.py
@@ -2,38 +2,51 @@
 #CS 454 Fall 2022
 #Assignment 2
 
-from ast import Dict
 import csv 
 
 class TF_IDF():
-    def __init__(self, dataFile):     
+    def __init__(self, dataFile):
+        self.dataFile = dataFile                   #CSV file passed to class object
+        self.DocumentCount = 0                     #Total document count in dataFile
+        self.TF: dict(str, dict(str, int)) = {}    #DIctionary holding TF values for each term and document
         self.DocumentTerms: dict(str, int) = {}    #Dictionary with # of terms for each document
-        self.Index = self.invertedIndex(dataFile)  #Inverted Index obtained from 
-              
+        self.Index = self.invertedIndex(dataFile)  #Inverted Index 
+
+        
     def tf_idf(self, Q, k):
         Query = Q.split()
         print(Query)
-        pass
+        for term in Query:
+            
+            for doc in self.Index[term]:
+              #  print(doc)
+                self.TF[doc][term] = self.tf(doc,term)
+       
+       # print(self.TF)
+       # print(self.Index)
     
     def relevance(self, d , Q):
-        pass
-    
-    
-    def tf(self, d, t):
         
         pass
+    
+    '''Returns TF score, the number of term occurrences in a document
+       divided by the total number of terms in document
+    '''
+    def tf(self, d, t):
+        return self.Index[t][d]/self.DocumentTerms[d] 
     
     '''Function which returns an inverted index "Index" for every term in the datafile.
        Also stores the number of terms for each document in dictionary "DocumentTerms"
     '''
     def invertedIndex(self, datafile):
-        Index: dict[str, dict[str, int]] = {}
+        Index: dict(str, dict(str, int)) = {}
         
         with open(datafile, 'r') as csvFile:   #Open and parse the CSV file
             csvreader = csv.reader(csvFile)
             next(csvreader)
-            for document in csvreader:  #Loop every document
-             #   print(document)
+            for document in csvreader:    #Loop every document
+                self.DocumentCount += 1        #Increment document count 
+                self.TF[document[0]] = {}
                 self.DocumentTerms[document[0]] = len(document[1].split()) #save the number of terms for each document
                 for term in document[1].split():  #For every term in document
                     
@@ -45,7 +58,6 @@ class TF_IDF():
         return Index
     
                     
-            
     
 
 class BM_25():
@@ -58,15 +70,11 @@ class BM_25():
         pass
 
 
-
-
 def main():
     file = "./wine.csv"    
     file2 = "winemag-data_first150k.csv"
     Rank = TF_IDF(file)
-    Rank.tf_idf("tremendous mac", 3)
-
-    pass
+    Rank.tf_idf("tremendous the", 3)
 
 if __name__ == '__main__':
 	main()

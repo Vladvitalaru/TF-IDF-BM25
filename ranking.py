7ddc802fa6d82cad035b718b9d2e0ba97a88ab25
Vlad <vladvitalaru@yahoo.com>
2022-09-16 20:58:58 -0700

    TF_IDF Implementation

--- ranking.py
+++ ranking.py
@@ -3,37 +3,76 @@
 #Assignment 2
 
 import csv 
+import math
 
 class TF_IDF():
     def __init__(self, dataFile):
-        self.dataFile = dataFile                   #CSV file passed to class object
-        self.DocumentCount = 0                     #Total document count in dataFile
-        self.TF: dict(str, dict(str, int)) = {}    #DIctionary holding TF values for each term and document
-        self.DocumentTerms: dict(str, int) = {}    #Dictionary with # of terms for each document
-        self.Index = self.invertedIndex(dataFile)  #Inverted Index 
+        self.dataFile = dataFile                         #CSV file passed to class object
+        self.DocumentCount = 0                           #Total document count in dataFile
+        self.TFscores: dict(str, dict(str, int)) = {}    #Dictionary holding TF_IDF values for each term and document
+        self.DocumentTerms: dict(str, int) = {}          #Dictionary with # of terms for each document
+        self.Index = self.invertedIndex(dataFile)        #Inverted Index of terms within document        
+        self.Results = []                                #Final list of documents and their TF_IDF values
 
-        
+    '''Returns a list of tuples representing the top k results based on tf_idf score'''
     def tf_idf(self, Q, k):
         Query = Q.split()
         print(Query)
-        for term in Query:
-            
-            for doc in self.Index[term]:
-              #  print(doc)
-                self.TF[doc][term] = self.tf(doc,term)
+        for term in Query: #Loop over every single term in Query
+            if term in self.Index:          #Check the index for the term
+                for doc in self.Index[term]:    
+                    self.TFscores[doc][term] = self.tfHelper(doc,term)      #Add TF values to dict
+                    if Query.count(term) > 1:      #If term occurs multiple times in query, multiply its tf value by the count
+                        self.TFscores[doc][term] = self.TFscores[doc][term] * Query.count(term)
+                    
+        self.TFsummed = self.TFscores.copy()    
+        for doc in self.TFsummed:   #Loop through TF scores for each document
+            if self.TFsummed[doc]:                      #Only calculated existing scores
+                
+                for terms in self.TFsummed[doc].keys():   #Loop through every document's term's TF values and apply the relevance function
+                    self.TFsummed[doc][terms] = (self.TFsummed[doc][terms]) / (self.numberOfDocuments(terms))  #Update with relevancy score
        
-       # print(self.TF)
-       # print(self.Index)
+        for doc in self.TFsummed:   #Go through the TF_IDF scores and sum them up for every document 
+            if self.TFsummed[doc]:
+                self.Results.append(( str(doc) , sum(self.TFsummed[doc].values())))
+                
+        self.Results.sort(reverse=True,key=lambda y: y[1]) #Sort the TF_IDF scores 
     
-    def relevance(self, d , Q):
-        
-        pass
+        count = 0
+        while count < k:
+            if count < len(self.Results):
+                print(self.Results[count])
+                count+=1
+            else:
+                break
     
-    '''Returns TF score, the number of term occurrences in a document
-       divided by the total number of terms in document
+    '''Prints TF score, Log( 1 + the number of term occurrences in a document
+       divided by the total number of terms in document)
     '''
     def tf(self, d, t):
-        return self.Index[t][d]/self.DocumentTerms[d] 
+        print(math.log( 1 + (self.Index[t][d]/self.DocumentTerms[d])))
+         
+    ''' Returns relevance score for a particular query in a document
+    '''
+    def relevance(self, d, Q):
+        for term in Q.split():
+            if term in self.Index:          #Check the index for the term   
+                self.TFscores[d][term] = self.tfHelper(d,term)      #Add TF values to dict
+                if Q.count(term) > 1:
+                    self.TFscores[d][term] = self.TFscores[d][term] * Q.count(term) #If term is repeated, multiply its TF value
+                    
+        total = 0
+        for term in self.TFscores[d]:
+            total += self.TFscores[d][term] / self.numberOfDocuments(term)
+            
+        print(total)
+        
+    
+    '''Returns TF score, Log( 1 + the number of term occurrences in a document
+       divided by the total number of terms in document)
+    '''
+    def tfHelper(self, d, t):
+        return math.log( 1 + (self.Index[t][d]/self.DocumentTerms[d])) 
     
     '''Function which returns an inverted index "Index" for every term in the datafile.
        Also stores the number of terms for each document in dictionary "DocumentTerms"
@@ -46,23 +85,30 @@ class TF_IDF():
             next(csvreader)
             for document in csvreader:    #Loop every document
                 self.DocumentCount += 1        #Increment document count 
-                self.TF[document[0]] = {}
-                self.DocumentTerms[document[0]] = len(document[1].split()) #save the number of terms for each document
-                for term in document[1].split():  #For every term in document
+                self.TFscores[document[0]] = {}
+                self.DocumentTerms[document[0]] = len(document[1].split()) #save the number of terms for each doc
+                for term in document[1].split():  #For every term in doc
                     
-                    if term in Index:    #Check if term was seen before
-                        Index[term][document[0]] =+ 1
-                    else:
+                    if term not in Index:    #Check if term was seen before
                         Index[term] = {}
-                        Index[term][document[0]] = 1
+                        Index[term][document[0]] = 1   
+                    else:
+                        if document[0] in Index[term]:      #Check if the term has been seen before in the current doc
+                            Index[term][document[0]] += 1
+                        else:
+                            Index[term][document[0]] = 1
         return Index
     
-                    
+    '''Returns the number of times a term appears in documents
+       Used to divide the TF scores for every term and document
+    '''
+    def numberOfDocuments(self, term):            
+        return len(self.Index[term])    
+           
     
 
 class BM_25():
     def __init__(self, dataFile):
-        
         pass
     
     
@@ -71,10 +117,18 @@ class BM_25():
 
 
 def main():
-    file = "./wine.csv"    
+    file = "wine.csv"    
     file2 = "winemag-data_first150k.csv"
     Rank = TF_IDF(file)
-    Rank.tf_idf("tremendous the", 3)
+ #   Rank.tf_idf("tremendous", 5)
+   # Rank.tf_idf("mac watson", 5)
+    Rank.tf_idf("tremendous tremendous watson", 5)
+
+   # Rank.tf("31", "and")
+  #  Rank.relevance("0", "tremendous")
+    Rank.relevance("2", "mac watson")
+    Rank.relevance("2", "mac mac watson")
+
 
 if __name__ == '__main__':
 	main()
